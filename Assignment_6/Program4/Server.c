d2b7b470912b87a982fefd919a3773be6333c7df
Ishwari Khopade <ishwarikhopade68@gmail.com>
2026-02-02 14:52:25 +0530

    Update Server.c

--- Assignment_6/Program4/Server.c
+++ Assignment_6/Program4/Server.c
@@ -1,1 +1,26 @@
+int Addition(int A, int B)
+{
+    int Ans = 0;
+    Ans = A + B;
+    return Ans;
+}
+int Subtraction(int A, int B)
+{
+    int Ans = 0;
+    Ans = A - B;
+    return Ans;
+}
+int Multiplication(int A, int B)
+{
+    int Ans = 0;
+    Ans = A * B;
+    return Ans;
+}
+int Division(int A, int B)
+{
+    int Ans = 0;
+    Ans = A / B;
+    return Ans;
+}
+
 
