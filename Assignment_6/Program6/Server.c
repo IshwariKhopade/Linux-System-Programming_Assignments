16467c85fad766376b10e410f7198a91ad8785dc
Ishwari Khopade <ishwarikhopade68@gmail.com>
2026-02-02 22:41:47 +0530

    Update Server.c

--- Assignment_6/Program6/Server.c
+++ Assignment_6/Program6/Server.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+void hello()
+{
+    printf("Shared Library\n");
+}
+
+
+
+//gcc -fPIC -c Server.c 
+//gcc -shared -o libserver.so Server.o 
+
+
+//for custom dir
+//mkdir mylibdir
+//mv libserver.so serverlib/
