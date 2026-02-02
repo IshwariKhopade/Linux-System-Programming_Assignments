518307c9a9b2add017832cc144cff06306be7a31
Ishwari Khopade <ishwarikhopade68@gmail.com>
2026-02-02 19:09:59 +0530

    Add basic arithmetic functions and CallMod

--- Assignment_6/Program5/Server.c
+++ Assignment_6/Program5/Server.c
@@ -1,1 +1,34 @@
+extern int Mod(int, int);
+
+int Addition(int A, int B)
+{
+    int Ans = 0;
+    Ans = A + B;
+    return Ans;
+}
+int Subtraction(int A, int B)
+{
+    int Ans = 0;
+    Ans = A - B;
+    return Ans;
+}
+int Multiplication(int A, int B)
+{
+    int Ans = 0;
+    Ans = A * B;
+    return Ans;
+}
+int Division(int A, int B)
+{
+    int Ans = 0;
+    Ans = A / B;
+    return Ans;
+}
+
+int CallMod(int No1, int No2)
+{
+    // This will cause symbol resolution
+    return Mod(No1, No2);
+}
+
 
